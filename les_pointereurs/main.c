7c87dbcb1fb97d2e782793d356bf96f924cce6c9
Fabrice-sangwa <19sm476@esisalama.org>
2022-11-01 20:54:28 +0200

    Initialisation de l'exercices sur les pointeurs

---
+++ les_pointereurs/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    
+ 
+    int x = 0; 
+
+    printf("X=%d\n", x);
+
+    int *p = NULL; 
+    p = &x;
+
+    printf("Valeur sur la quelle pointe p=%d\n", *p); 
+
+    *p = 44; 
+
+    printf("X=%d\n", x); 
+
+    printf("Valeur sur la quelle pointe p=%d", *p); 
+    //printf("%p\n", p);
+
+    return 0;
+}
