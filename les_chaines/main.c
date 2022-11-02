f7687abf0b66891925c8b93cd6f958337c292087
Fabrice-sangwa <19sm476@esisalama.org>
2022-11-02 20:58:59 +0200

    Utilisation de la bibiothèque string.h

--- les_chaines/main.c
+++ les_chaines/main.c
@@ -13,7 +13,25 @@ int main(int argc, char *argv[]){
     printf("%s\n", phrase); 
 
 
-    printf("La taille de la chaine Bonjour = %d", strlen("Bonjour")); 
+    printf("La taille de la chaine Bonjour = %d\n", strlen("Bonjour")); 
+
+    char chaine1[255] = " James"; 
+    char chaine2[] = "Salut"; 
+
+     
+
+    printf("Chaine 1 = %s\n", chaine1); 
+    printf("Chaine 2 = %s\n", chaine2); 
+
+    printf("--------------------\n"); 
+
+    strcat(chaine2, chaine1);
+
+    printf("Chaine 2 = %s\n", chaine2);
+
+    printf("--------------------\n"); 
+
+    
 
     return 0; 
     
