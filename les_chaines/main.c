ced16fc3a52a81d7c73a7e1c84915364a98a2e32
Fabrice-sangwa <19sm476@esisalama.org>
2022-11-02 19:19:09 +0200

    Utilisation des chaîne de caractère

---
+++ les_chaines/main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+
+
+    char maChaine[] = "Salut"; 
+    char *phrase = "Salut je vais bien"; 
+    
+    printf("%s\n", maChaine); 
+    printf("%s\n", phrase); 
+
+
+    printf("La taille de la chaine Bonjour = %d", strlen("Bonjour")); 
+
+    return 0; 
+    
+}
