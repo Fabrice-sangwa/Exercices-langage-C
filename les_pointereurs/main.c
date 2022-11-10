b08fb0d6ed7203e10679f033f201cc21f28ea4c6
Fabrice-sangwa <19sm476@esisalama.org>
2022-11-10 15:02:17 +0200

    Les exercices basiques avec quelques exercices sur les pointeurs

--- les_pointereurs/main.c
+++ les_pointereurs/main.c
@@ -2,25 +2,43 @@
 #include <stdio.h>
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     
  
-    int x = 0; 
 
-    printf("X=%d\n", x);
+    //on crée une variable 
+    int age = 70;
 
-    int *p = NULL; 
-    p = &x;
+    //on crée un pointeur que l'on initialise à NULL
+    int *monPointeur = NULL; 
 
-    printf("Valeur sur la quelle pinte p=%d\n", *p); 
+    //on associe l'adresse de la variable (age) au pointeur (monPointeur)
+    monPointeur = &age;
 
-    *p = 44; 
+    //onaffiche la valeur de la variable age 
+    printf("Age = %d\n", age); 
 
-    printf("X=%d\n", x); 
+    //on affiche l'adresse de la variable 
+    printf("Adresse age = %p\n", &age); 
 
-    printf("Valeur sur la quelle pointe p=%d", *p); 
-    //printf("%p\n", p);
+    //on affiche la valeur du pointeur monPointeur  
+    printf("Adresse dans monPointeur = %p\n", monPointeur); 
+
+    //on affiche la valeur du pointeur monPointeur  en decimal
+    printf("Adresse  dans monPointeur decimal = %d\n", monPointeur); 
+
+
+    //on affiche la valeur qui est sur l'adresse monPointeur
+    printf("Valeur contenu dans monPointeur = %d\n", *monPointeur); 
+
+    //Modifier la valeur de la variable age grâce au pointeur 
+    *monPointeur = 19;
+
+    //on affiche la valeur de la variable age
+    printf("Adresse age = %d\n", age); 
+
+
+
 
     return 0;
 }
