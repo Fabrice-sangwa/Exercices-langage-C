a5de7bf85f275805f21aedfc134944e0a525a2bb
Fabrice-sangwa <19sm476@esisalama.org>
2022-11-10 23:19:00 +0200

    Quelques exercices sur les tableaux

--- les_tableaux/main.c
+++ les_tableaux/main.c
@@ -1,49 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int incremente();
+void parcourir(int *tableau, int taille); 
  
 int main(int argc, char *argv[]){
     
     //Déclartion d'un tableau 
     int tableau[6];
 
+    int taille = 6; 
+
     //Affectation des valeurs 
 
     tableau[0] = 12; 
     tableau[1] = -3;
-    tableau[2] = 12;
+    tableau[2] = 54;
     tableau[3] = 0;
     tableau[4] = -32;
     tableau[5] = 1;
 
-    for (int i = 0; i<6; i++){
-        printf("L'element: %d\n", tableau[i]); 
-
-    }
-
-    printf("Jell\n"); 
-   
-    //Affichage du premier élément du tableau
-    printf("%d\n", *tableau); 
-
-    // Aficher l'adresse du tableau 
+    //par defaut le tableau est un un pointeur sur le premier élément 
     printf("%d\n", tableau); 
 
-    //Afficher la valeur du deuxième élément du tableau 
-    printf("%d\n", *(tableau + 1)); 
-
-
-    //Création d'un tableau dynamique
-    char tableau1[] = {'A', 'B', 'D'};
-    printf("%c\n", *tableau1); 
-
-    //Parcours d'un tableau 
-
+    //Afficher le premier élement du tableau 
+    printf("%d\n", *tableau); 
     
+    //Afficher le troisième élement du tableau
+    printf("%d\n", *(tableau + 2)); 
 
-
-
+    //Parcourir un tableau 
+    parcourir(tableau, 6); 
 
     return 0;
 }
+
+void parcourir(int *tableau, int taille){
+
+    int i; 
+
+    printf("--------Le Tableau----------");
+    for(i = 0; i<taille; i++){
+        printf("%d\n", *(tableau + i)); 
+    }
+} 
